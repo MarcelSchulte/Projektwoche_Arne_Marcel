cdee6b84133d8e3ef5353a25dfc29cc3c493a67e
MarcelSchulte <marcelschulte99@gmx.de>
2018-02-28 11:03:44 +0100

    1.

---
+++ Monitoring_Projektwoche_Arne_Marcel/Monitoring_Projektwoche_Arne_Marcel/frmMonitoring.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monitoring_Projektwoche_Arne_Marcel
+{
+    public partial class frmMonitoring : Form
+    {
+        public frmMonitoring()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Test_Projekt/Test_Projekt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Test_Projekt
+{
+    class Program
+    {
+        static PerformanceCounter CPU;
+        static PerformanceCounter RAM;
+
+        static void Main(string[] args)
+        {
+            CPU = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            RAM = new PerformanceCounter("Memory", "Available MBytes");
+
+
+            while (true)
+            {
+                Console.WriteLine(GetRAM());
+                Console.WriteLine(GetCPU());
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+
+        static string GetCPU()
+        {
+            return CPU.NextValue() + "%";
+        }
+
+        static string GetRAM()
+        {
+            return RAM.NextValue() + "MB";
+        }
+
+    }
+}
